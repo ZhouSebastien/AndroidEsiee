5032224e3a44d78e310bb5bf23eea755e6040dfd
Zhou Sebastien <sebastien.zhou@edu.esiee.fr>
2020-02-27 08:34:59 +0100

    create static list of jokes

--- ChuckNorrisApp/app/src/main/java/com/example/chucknorrisapp/MainActivity.kt
+++ ChuckNorrisApp/app/src/main/java/com/example/chucknorrisapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.chucknorrisapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import values.jokesList
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val chuckJoke = jokesList.jokes
+        Log.i("Jokes Object", chuckJoke.toString())
     }
+
 }
